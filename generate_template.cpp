51049eac9ab0bd1ea34c9999f5da44b0f2fb8031
nauynix <nauynix@gmail.com>
2021-03-20 21:06:57 +0800

    Removed error debug because not using

--- generate_template.cpp
+++ generate_template.cpp
@@ -7,13 +7,6 @@ using namespace std;
 #define pb push_back
 #define fi first
 #define se second
-#define error(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
-void err(istream_iterator<string> it) {}
-template<typename T, typename... Args>
-void err(istream_iterator<string> it, T a, Args... args) {
-	cerr << *it << " = " << a << endl;
-	err(++it, args...);
-}
 typedef pair<int, int> PII;
 typedef vector<int> VI;
 typedef vector<string> VS;
